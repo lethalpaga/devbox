72fec010b4d7795cd0608c1ff218d1c82d563b0c
lethalpaga <lethalpaga@gmail.com>
2016-04-04 10:14:45 +1200

    Customize vimrc

--- cookbooks/devbox/attributes/vim.rb
+++ cookbooks/devbox/attributes/vim.rb
@@ -1,2 +1,5 @@
 default['vim_config']['bundle_dir'] = "/home/#{node['devbox']['user']}/.vim/bundle"
 default['vim_config']['plugin_manager'] = 'pathogen'
+default['devbox']['vim']['colorscheme'] = 'base16-default'
+default['devbox']['vim']['guifont'] = 'Inconsolata\ for\ Powerline\ Medium\ 11'
+default['devbox']['vim']['background'] = 'dark'
