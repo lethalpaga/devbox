c8f0e45ab3cb17bbc2b6d3576b7d7d11eb03c87e
lethalpaga <lethalpaga@gmail.com>
2016-04-08 17:01:57 +1200

    Split Xresources in multiple files

--- cookbooks/devbox/recipes/base16.rb
+++ cookbooks/devbox/recipes/base16.rb
@@ -10,13 +10,33 @@ directory destination do
   recursive true
 end
 
+xresources_dir = "#{home_dir}/.Xresources.d"
+directory xresources_dir do
+  user username
+  group groupname
+end
+
 git destination do
   repository repo
 end
 
-remote_file "#{home_dir}/.Xresources" do 
+# Main Xresources
+template "#{home_dir}/.Xresources" do
+  user username
+  group groupname
+  source 'Xresources/.Xresources'
+end
+
+# Additional Xresources
+remote_file "#{xresources_dir}/color_theme" do
   source "file://#{destination}/#{node['devbox']['base16']['xresources']}"
-  owner 'root'
-  group 'root'
+  owner username
+  group groupname
   mode 0755
+end
+
+template "#{xresources_dir}/rxvt" do
+  source 'Xresources/rxvt'
+  user username
+  group groupname
 end
