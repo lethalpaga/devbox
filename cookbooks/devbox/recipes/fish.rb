e90407667c551dd5970b32beb6663d5f0fd0804a
lethalpaga <lethalpaga@gmail.com>
2016-04-16 12:55:04 +1200

    Fish fundle installation

--- cookbooks/devbox/recipes/fish.rb
+++ cookbooks/devbox/recipes/fish.rb
@@ -14,17 +14,12 @@ directory "#{fish_dir}/functions" do
   group groupname
 end
 
-template "#{fish_dir}/config.fish" do
-  source 'fish/config.fish'
-  user username
-  group groupname
-end
-
 execute 'install_fundle' do
   command 'fish -c \'curl -sfL https://git.io/fundle-install | fish\''
   user username
   not_if do ::File.exists?("#{home_dir}/.config/fish/functions/fundle.fish") end
   environment({
+    "HOME" => home_dir,
     "XDG_DATA_HOME" => "#{home_dir}/.local/share",
     "XDG_CONFIG_HOME" => "#{home_dir}/.config"
   })
@@ -32,6 +27,12 @@ end
 
 fundle_dir = "#{home_dir}/.config/fundle"
 directory fundle_dir do
+  user username
+  group groupname
+end
+
+template "#{fish_dir}/config.fish" do
+  source 'fish/config.fish'
   user username
   group groupname
 end
