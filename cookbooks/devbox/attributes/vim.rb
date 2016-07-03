847bcf7134c7221f7743c1f84e24fdb4ce6d4a55
lethalpaga <lethalpaga@gmail.com>
2016-07-04 09:42:03 +1200

    Fix vimrc for the new base16

--- cookbooks/devbox/attributes/vim.rb
+++ cookbooks/devbox/attributes/vim.rb
@@ -1,5 +1,5 @@
 default['vim_config']['bundle_dir'] = "/home/#{node['devbox']['user']}/.vim/bundle"
 default['vim_config']['plugin_manager'] = 'pathogen'
-default['devbox']['vim']['colorscheme'] = 'base16-default'
+default['devbox']['vim']['colorscheme'] = 'base16-default-dark'
 default['devbox']['vim']['guifont'] = 'Inconsolata'
 default['devbox']['vim']['background'] = 'dark'
