1914ab0f03db8e97d037365674cf93c93a2d395c
lethalpaga <lethalpaga@gmail.com>
2020-05-18 08:48:38 +1200

    Update

--- cookbooks/devbox/templates/default/fish/config.d/k8s.fish
+++ cookbooks/devbox/templates/default/fish/config.d/k8s.fish
@@ -1,25 +1,29 @@
+[ -z "$KUBECONFIG" ]; and set -x KUBECONFIG $HOME/.kube/config
+
 alias k='kubectl'
 alias ka='kubectl apply'
 alias kc='kubectl create'
 alias kd='kubectl delete'
 alias ke='kubectl edit'
 alias kde='kubectl describe'
+alias wkg='watch -d -- kubectl get'
 alias kg='kubectl get'
 alias kga='kubectl get --all-namespaces'
 alias kl='kubectl logs'
 alias klf='kubectl logs -f --tail=10'
 alias kgp='kubectl get pods'
-alias wkgp='watch "kubectl get pods"'
+alias wkgp='watch -d -- kubectl get pods'
 alias kgs='kubectl get services'
-alias wkgs='watch "kubectl get services"'
+alias wkgs='watch -d -- kubectl get services'
 alias kgd='kubectl get deployments'
-alias wkgd='watch "kubectl get deployments"'
+alias wkgd='watch -d -- kubectl get deployments'
 alias kge='kubectl get events --sort-by=".lastTimestamp"'
+alias wkge='watch -d -- kubectl get events --sort-by=".lastTimestamp"'
 alias kpf='kubectl port-forward'
 alias kcgc='kubectl config get-contexts'
 alias kcuc='kubectl config use-context'
 
-alias wa='watch -x fish -c'
+alias wa='watch -d -x fish -c'
 
 alias get-pod="kubectl get pods -o jsonpath='{.items[0].metadata.name}'"
 alias gp='get-pod'
